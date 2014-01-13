2ad904bc83baf8a4a06ae79181be2212132e2173
Roman Laukhin <r.laukhin@gmail.com>
2014-01-13 14:47:47 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleTickets::Application.config.secret_key_base = '879eaad1305007132eb025387b8a05fc1569d4de5478d3d20067ab902f16bdd55bf571d882c20bbbd35117280452b0db7c9bd3e2d77e00b125943d477faf8569'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleTickets::Application.config.session_store :cookie_store, key: '_simple_tickets_session'
